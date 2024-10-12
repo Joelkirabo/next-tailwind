e87c41f5fb39029ba391db03f9ed1a767ffb9fa0
joel kirabo <kirabojoel001@gmail.com>
2024-10-12 19:25:42 +0300

    changed the fetch url

--- components/Bloghome.jsx
+++ components/Bloghome.jsx
@@ -4,11 +4,11 @@ import Blog from "./Blog"
 import useFetch from "./useFetch"
 
 export default function Bloghome(){
-    const {data:blogs, isLoading, error}= useFetch('http://localhost:8000/blogs') 
+    const {data:blogs, isLoading, error}= useFetch('https://github.com/Joelkirabo/next-tailwind/blob/main/data/db.json') 
    
     return (
         <div className="p-4">
-            
+
             {error&&<div>{error}</div>}
             {isLoading&&<div className='flex justify-center'>Data Loading....</div>}
             {blogs&&<Blog blogs={blogs} title="All blogs" />}   
